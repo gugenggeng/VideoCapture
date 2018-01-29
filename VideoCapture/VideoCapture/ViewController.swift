73a5f0f657010d1255528f4eb23493bfa6a9fcb9
顾耿耿 <353472534@qq.com>
2018-01-29 11:00:37 +0800

    上传代码

---
+++ VideoCapture/VideoCapture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoCapture
+//
+//  Created by 顾耿耿 on 2018/1/29.
+//  Copyright © 2018年 顾耿耿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
